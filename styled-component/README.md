3ad74bd6074c84a9f363db5b26a9064b5cfcd76f
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-12-20 01:29:54 +0900

    Basic styled-components

--- styled-component/README.md
+++ styled-component/README.md
@@ -1,3 +1,8 @@
+## extension (vsc)
+
+- styled-components (auto)
+- https://marketplace.visualstudio.com/items?itemName=diegolincoln.vscode-styled-components
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- styled-component/src/App.js
+++ styled-component/src/App.js
@@ -1,5 +1,112 @@
+import styled, { keyframes } from 'styled-components';
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+// styled(A)
+// A 속성그대로 + 추가옵션
+const Circle = styled(Box)`
+  background-color: ${(props) => props.bgColor};
+  border-radius: 50px;
+`;
+
+// 디자인을 그대로 컴포넌트 속성만 바꾸고 싶을때, as 이용 ( 사용하는 곳에서 )
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border-radius: 15px;
+  border: 0;
+`;
+
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const rotationAnimation = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius:0px;
+  }
+  50% {
+    border-radius:100px;
+  }
+  100%{
+    transform:rotate(360deg);
+    border-radius:0px;
+  }
+`;
+
+// 자식의 엘리먼트 속성을 지정할 수 있음
+const Square = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: pink;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 48px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
+`;
+
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+
+// Emoji 를 컴포넌트가 다른속성(as) 으로 바껴도 적용된다.
+// Pseudo Selector
+const Square2 = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: pink;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1s linear infinite;
+  ${Emoji} {
+    font-size: 36px;
+    &:hover {
+      font-size: 48px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
+`;
+
 function App() {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <Square>
+        <span>😍</span>
+      </Square>
+      <Square2>
+        <Emoji as='a'>🥶</Emoji>
+      </Square2>
+    </Wrapper>
+  );
 }
 
 export default App;
