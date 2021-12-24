9c1dbdd758433fb1ab01202304aaee3609d77ae1
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-12-24 23:56:35 +0900

    Nested Route

--- styled-component/src/App.tsx
+++ styled-component/src/App.tsx
@@ -58,9 +58,11 @@ table {
 }  
 
 body {
+  font-weight: 300;
   font-family: 'Source Sans Pro', sans-serif;
   background-color: ${(props) => props.theme.bgColor};
-  color: ${(props) => props.theme.textColor}; 
+  color: ${(props) => props.theme.textColor};
+  line-height: 1.2;
 }
 
 a {
