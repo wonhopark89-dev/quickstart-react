711f6c9c19ac780138562d938e9ded3e5723e03f
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-02-05 02:43:54 +0900

    Advanced useForm, validation

--- todo-app/src/ToDoList.tsx
+++ todo-app/src/ToDoList.tsx
@@ -33,17 +33,33 @@ import {useForm} from 'react-hook-form';
 
 // react-hook-form 활용
 const ToDoList = () => {
-  const {register, watch} = useForm();
+  const {register, watch, handleSubmit, formState} = useForm();
   // console.log(register('test')); // {name: 'test', onChange: ƒ, onBlur: ƒ, ref: ƒ}
+
+  const onValid = (data: any) => {
+    // console.log(data);
+  };
+
+  console.log(formState.errors);
+
   return (
     <div>
-      <form>
-        <input {...register('email')} placeholder={'Email'} />
-        <input {...register('firstName')} placeholder={'First Name'} />
-        <input {...register('lastName')} placeholder={'Last Name'} />
-        <input {...register('username')} placeholder={'Username'} />
-        <input {...register('password')} placeholder={'Password'} />
-        <input {...register('password')} placeholder={'Password1'} />
+      <form style={{display: 'flex', flexDirection: 'column'}} onSubmit={handleSubmit(onValid)}>
+        <input {...register('email', {required: true})} placeholder={'Email'} />
+        <input {...register('firstName', {required: true})} placeholder={'First Name'} />
+        <input {...register('lastName', {required: true})} placeholder={'Last Name'} />
+        <input {...register('username', {required: true, minLength: 5})} placeholder={'Username'} />
+        <input {...register('password', {required: true, minLength: 5})} placeholder={'Password'} />
+        <input
+          {...register('password', {
+            required: true,
+            minLength: {
+              value: 5,
+              message: 'Your password is too short',
+            },
+          })}
+          placeholder={'Password1'}
+        />
         <button>Add</button>
       </form>
     </div>
@@ -51,3 +67,7 @@ const ToDoList = () => {
 };
 
 export default ToDoList;
+
+// handleSubmit 은 성공,실패 각 callback 을 설정할 수 있음 ( 성공값은 필수 )
+// required 를 html-input 컴포넌트에 적용하면 html 에서 체크해줌
+// -> 지원하지않은 브라우저나, 커스텀을 하기위해 속성으로 적용
