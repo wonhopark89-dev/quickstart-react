4bc72c2106c3e3d673c89d0124398abc7f0ce73f
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-02-05 00:18:34 +0900

    Add simple react-hook-form

--- todo-app/src/ToDoList.tsx
+++ todo-app/src/ToDoList.tsx
@@ -1,22 +1,49 @@
 import React, {useState} from 'react';
+import {useForm} from 'react-hook-form';
 
+// const ToDoList = () => {
+//   const [todo, setTodo] = useState<string>('');
+//   const [todoError, setToDoError] = useState<string>('');
+//
+//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: {value},
+//     } = event;
+//     setToDoError('');
+//     setTodo(value);
+//   };
+//
+//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     if (todo.length < 10) {
+//       return setToDoError('To do should be longer');
+//     }
+//   };
+//
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input onChange={onChange} value={todo} placeholder={'Write a to-do'} />
+//         <button>Add</button>
+//         {todoError !== '' ? todoError : null}
+//       </form>
+//     </div>
+//   );
+// };
+
+// react-hook-form 활용
 const ToDoList = () => {
-  const [todo, setTodo] = useState<string>('');
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: {value},
-    } = event;
-    setTodo(value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-  };
-
+  const {register, watch} = useForm();
+  // console.log(register('test')); // {name: 'test', onChange: ƒ, onBlur: ƒ, ref: ƒ}
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={todo} placeholder={'Write a to-do'} />
+      <form>
+        <input {...register('email')} placeholder={'Email'} />
+        <input {...register('firstName')} placeholder={'First Name'} />
+        <input {...register('lastName')} placeholder={'Last Name'} />
+        <input {...register('username')} placeholder={'Username'} />
+        <input {...register('password')} placeholder={'Password'} />
+        <input {...register('password')} placeholder={'Password1'} />
         <button>Add</button>
       </form>
     </div>
