3da8576361383896b7f08cb583851b55d4e23eb6
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-02-16 01:25:52 +0900

    Movement in cross-board

--- todo-app/src/components/Dnd.tsx
+++ todo-app/src/components/Dnd.tsx
@@ -33,6 +33,7 @@ const Dnd = () => {
     if (destination?.droppableId === source.droppableId) {
       // same board movement.
       setToDos(allBoards => {
+        // 수정이 발생한 보드만 복사
         const boardCopy = [...allBoards[source.droppableId]];
         boardCopy.splice(source.index, 1);
         boardCopy.splice(destination?.index, 0, draggableId);
@@ -40,6 +41,17 @@ const Dnd = () => {
           ...allBoards,
           [source.droppableId]: boardCopy,
         };
+      });
+    }
+
+    if (destination?.droppableId !== source.droppableId) {
+      // cross board movement.
+      setToDos(allBoards => {
+        const sourceBoard = [...allBoards[source.droppableId]];
+        const destinationBoard = [...allBoards[destination.droppableId]];
+        sourceBoard.splice(source.index, 1);
+        destinationBoard.splice(destination.index, 0, draggableId);
+        return {...allBoards, [source.droppableId]: sourceBoard, [destination.droppableId]: destinationBoard};
       });
     }
   };
